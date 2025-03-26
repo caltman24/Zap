6e9a3b9b70b1a911511da18891e190640c70f932
Corbyn Altman <corbyn.altman01@gmail.com>
2025-03-26 18:24:39 -0400

    add update company info endpoint

--- server/Zap.Api/Endpoints/CompanyEndpoints.cs
+++ server/Zap.Api/Endpoints/CompanyEndpoints.cs
@@ -1,9 +1,13 @@
 ﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Zap.Api.Extensions;
 using Zap.DataAccess;
+using Zap.DataAccess.Constants;
 using Zap.DataAccess.Models;
+using Zap.DataAccess.Services;
 
 namespace Zap.Api.Endpoints;
 
@@ -14,37 +18,78 @@ public static class CompanyEndpoints
         var group = app.MapGroup("/company");
 
         group.MapGet("/info",
-                async Task<Results<BadRequest<string>, Ok<CompanyInfoResponse>>> (AppDbContext db,
-                    UserManager<AppUser> userManager, HttpContext context) =>
+            async Task<Results<BadRequest<string>, Ok<CompanyInfoResponse>>> (AppDbContext db,
+                UserManager<AppUser> userManager, HttpContext context) =>
+            {
+                var user = await userManager.GetUserAsync(context.User);
+                if (user == null) return TypedResults.BadRequest("User not found");
+
+                // Load company with members in a single query
+                var company = await db.Companies
+                    .Include(c => c.Members)
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(c => c.Id == user.CompanyId);
+
+                if (company == null) return TypedResults.BadRequest("Company not found");
+
+                var membersByRole = new Dictionary<string, List<MembersResponse>>();
+
+                foreach (var member in company.Members)
                 {
+                    var roles = await userManager.GetRolesAsync(member);
+                    var role = roles.FirstOrDefault() ?? "None";
+                    if (!membersByRole.TryGetValue(role, out var value))
+                    {
+                        value = [];
+                        membersByRole[role] = value;
+                    }
+
+                    value.Add(new MembersResponse(member.FirstName + " " + member.LastName, member.AvatarUrl));
+                }
+
+                return TypedResults.Ok(new CompanyInfoResponse(company.Name, company.Description, membersByRole));
+            }).RequireAuthorization();
+
+        // Update company info with multipart form data.
+        // file can be null
+        // can also remove the image
+        group.MapPost("/info",
+                async Task<Results<BadRequest<string>, NoContent>> (HttpContext context, IFileUploadService fileUploadService, UserManager<AppUser> userManager,
+                    AppDbContext db) =>
+                {
+                    if (!context.Request.HasFormContentType)
+                    {
+                        return TypedResults.BadRequest("Invalid content type.  " +
+                                                  "Must be multipart/form-data.");
+                    }
+
+                    var (file, upsertCompanyInfoRequest) = context.ParseMultipartForm<UpsertCompanyInfoRequest>();
+
                     var user = await userManager.GetUserAsync(context.User);
                     if (user == null) return TypedResults.BadRequest("User not found");
 
-                    // Load company with members in a single query
-                    var company = await db.Companies
-                        .Include(c => c.Members)
-                        .AsNoTracking()
-                        .FirstOrDefaultAsync(c => c.Id == user.CompanyId);
-
+                    var company = await db.Companies.FindAsync(user.CompanyId);
                     if (company == null) return TypedResults.BadRequest("Company not found");
-
-                    var membersByRole = new Dictionary<string, List<MembersResponse>>();
-
-                    foreach (var member in company.Members)
+                    if (upsertCompanyInfoRequest.RemoveLogo)
                     {
-                        var roles = await userManager.GetRolesAsync(member);
-                        var role = roles.FirstOrDefault() ?? "None";
-                        if (!membersByRole.TryGetValue(role, out var value))
-                        {
-                            value = [];
-                            membersByRole[role] = value;
-                        }
-
-                        value.Add(new MembersResponse(member.FirstName + " " + member.LastName, member.AvatarUrl));
+                        company.LogoUrl = null;
                     }
 
-                    return TypedResults.Ok(new CompanyInfoResponse(company.Name, company.Description, membersByRole));
-                }).RequireAuthorization();
+                    if (file != null)
+                    {
+                        company.LogoUrl = await fileUploadService.UploadAvatarAsync(file);
+                    }
+
+                    company.Name = upsertCompanyInfoRequest.Name;
+                    company.Description = upsertCompanyInfoRequest.Description;
+                    company.WebsiteUrl = upsertCompanyInfoRequest.WebsiteUrl;
+
+                    db.Companies.Update(company);
+                    await db.SaveChangesAsync();
+
+                    return TypedResults.NoContent();
+                })
+            .RequireAuthorization(pb => { pb.RequireRole(RoleNames.Admin); });
 
         return app;
     }
@@ -53,3 +98,5 @@ public static class CompanyEndpoints
 public record MembersResponse(string Name, string AvatarUrl);
 
 public record CompanyInfoResponse(string Name, string Description, Dictionary<string, List<MembersResponse>> Members);
+
+public record UpsertCompanyInfoRequest(string Name, string Description, bool RemoveLogo, string? WebsiteUrl);
